57cc9f40edbf2200813e2c281866ad913bb924df
Adam Rosenblum <adamrosenblum@Adams-MBP.attlocal.net>
2017-08-20 20:51:48 -0700

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,29 @@
 require('babel-polyfill');
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+
+
+import ContactContainer from './components/contact-container';
+
+const routes = (
+    <Router history={hashHistory}>
+        <Route path="/contacts" component={App}>
+            <IndexRoute component={ContactListContainer} />
+            <Route path=":contactId" component={ContactContainer} />
+        </Route>
+    </Router>
+);
+
+// import ContactListContainer from './components/contact-list-container';
+
+// const routes = (
+//   <Router history={hashHistory}>
+//     <Route path="/contacts" component={ContactListContainer} />
+//   </Router>
+// );
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(routes, document.getElementById('app'))
+);
